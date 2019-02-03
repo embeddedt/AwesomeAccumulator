b0b3c4291571fba4580e8b45ab758ea4d1787a87
embeddedt <embeddedthemba@gmail.com>
2019-02-03 10:43:52 -0500

    Initial commit

---
+++ sequencer.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Intro scene constructor function
+function Intro()
+{
+    this.setup = function() {
+    };
+
+    this.draw = function() {
+    };
+
+    this.keyPressed = function() {
+        // switch the scene
+        this.sceneManager.showScene( Game );
+    };
+}
+
+// Main games scene constructor function
+function Game()
+{
+    this.setup = function() {
+    };
+
+    this.draw = function() {
+    };
+}
+
